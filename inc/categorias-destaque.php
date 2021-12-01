a6f5aa8680b9cc56126843eca37e7061162b211c
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2021-12-01 17:58:02 -0300

    categorias destaque // implementação

---
+++ inc/categorias-destaque.php
@@ -0,0 +1,53 @@
+<div class="categoria-destaque">
+
+    <div class="categoria-destaque-field">
+
+        <div class="cat-destaq">
+
+            <section>
+                <img src="assets/imagens/banners/mesa-industrial-1.png" alt="">
+            </section>
+
+            <section>
+                <h2>Nome da categoria</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos ducimus laborum volup.</p>
+                <br>
+                <a href="#">Saiba mais! >></a>
+            </section>
+
+        </div>
+
+        <div class="cat-destaq">
+
+            <section>
+                <img src="assets/imagens/banners/mesa-industrial-2.png" alt="">
+            </section>
+
+            <section>
+                <h2>Nome da categoria</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos ducimus laborum volup.</p>
+                <br>
+                <a href="#">Saiba mais! >></a>
+            </section>
+
+        </div>
+
+        <div class="cat-destaq">
+
+            <section>
+                <img src="assets/imagens/banners/mesa-industrial-3.png" alt="">
+            </section>
+
+            <section>
+                <h2>Nome da categoria</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos ducimus laborum volup.</p>
+                <br>
+                <a href="#">Saiba mais! >></a>
+            </section>
+
+        </div>
+
+
+    </div>
+
+</div>
--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <?php
     include('inc/header.php');
     include('inc/nav-menu.php');
-    include('inc/show-slider.php')
+    include('inc/show-slider.php');
+    include('inc/categorias-destaque.php')
 ?>
 
 <script src="assets/js/slider.js"></script>
