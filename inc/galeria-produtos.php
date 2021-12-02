23fd90ddc671e07edfd578495f9c3d331154da2a
Thomas Almeida <thomas.rodrigues@idealtrends.com.br>
2021-12-02 17:33:04 -0300

    Galeria de Produtos // implementação

---
+++ inc/galeria-produtos.php
@@ -0,0 +1,38 @@
+
+<div class="produtos-relacionados-titles">
+    <h2>Galeria de Produtos</h2>
+</div>
+
+
+<div class="galeria-produtos">
+    <div class="galeria-produtos-field">
+
+        <ul class="thumb-list">
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-1.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-2.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-3.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-2.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-1.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-3.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-2.png" alt="">
+            </li>
+            <li>
+                <img src="assets/imagens/banners/mesa-industrial-1.png" alt="">
+            </li>
+        </ul>
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     include('inc/nav-menu.php');
     include('inc/show-slider.php');
     include('inc/categorias-destaque.php');
-    include('inc/produtos-relacionados.php')
+    include('inc/produtos-relacionados.php');
+    include('inc/galeria-produtos.php')
 ?>
 
 <script src="assets/js/slider.js"></script>
